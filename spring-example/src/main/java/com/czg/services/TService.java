ebdda9c68f5e6ac682cbfa62a27db7d67cc30d9c
caozhigao <1183702890@qq.com>
2021-04-21 16:26:08 +0800

    first commit

---
+++ spring-example/src/main/java/com/czg/services/TService.java
@@ -0,0 +1,11 @@
+package com.czg.services;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class TService {
+	public static void main(String[] args) {
+		//初始化spring 容器
+		AnnotationConfigApplicationContext a = new AnnotationConfigApplicationContext();
+
+	}
+}
